38fe4c7bf655cbab0d5ab7842d1317dc8a5faaa2
Kiya Abdulahi <kiya.abdulahi@gmail.com>
2019-06-01 11:35:43 -0500

    limping map

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
+
+
     $("button").on("click", function (e) {
         e.preventDefault();
+
+        var lat;
+        var lng;
 
         // Clearing out table each time Submit is hit
         $(".table > tbody:last").children().remove();
@@ -10,7 +15,7 @@ $(document).ready(function () {
         var queryURL = "https://api.openbrewerydb.org/breweries?by_city=";
         $.ajax({
             url: queryURL + $(".search-index").val().trim(),
-            method: "GET"
+            method: "GET",
         }).then(function (response) {
 
             // Adding logic to return no results if there are no breweries in city searched for
@@ -21,32 +26,46 @@ $(document).ready(function () {
             }
 
             // create a new row for each brewery object returned by api + append to table
-            function displayBreweryInfo (response){
+            function displayBreweryInfo(response) {
 
-                for (var i =0; i<9; i++) {
-            
-                var brewery = response[i].name; 
-                var street = response[i].street;
-                var zip = response[i].postal_code; 
-                var state = response[i].state;
-                var address = street+", "+state+", "+zip; 
-                var number = response[i].phone;
-                var website = response[i].website_url; 
-            
-                var newRow = $("<tr>").append(
-                $("<td>").text(brewery),
-                $("<td>").text(address),
-                $("<td>").text(number),
-                $("<td>").html('<a href="'+website+'">'+website+'</a>'),
-                
-              );
-            
-              // Append the new row to the table
-              $(".table > tbody").append(newRow);
-            
-            
+                for (var i = 0; i < 9; i++) {
+
+                    var brewery = response[i].name;
+                    var street = response[i].street;
+                    var zip = response[i].postal_code;
+                    var state = response[i].state;
+                    var address = street + ", " + state + ", " + zip;
+                    var number = response[i].phone;
+                    var website = response[i].website_url;
+                    lat = parseFloat(response[i].latitude);
+                    console.log('lat', lat);
+                    lng = parseFloat(response[i].longitude);
+                    console.log('lng', lng)
+
+                    var newRow = $("<tr>").append(
+                        $("<td>").text(brewery),
+                        $("<td>").text(address),
+                        $("<td>").text(number),
+                        $("<td>").html('<a href="' + website + '">' + website + '</a>'),
+
+                    );
+
+                    // Append the new row to the table
+                    $(".table > tbody").append(newRow);
                 }
             }
         });
+        
+        function initMap() {
+            // The location of Uluru
+            var location = { lat: -25.344, lng: 131.036 };
+            // The map, centered at Uluru
+            var map = new google.maps.Map(
+                document.getElementById('map'), { zoom: 4, center: location });
+            // The marker, positioned at Uluru
+            var marker = new google.maps.Marker({ position: location, map: map });
+        }
+        initMap();
     });
+
 });
